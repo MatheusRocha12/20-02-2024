d859d4fbf0cd8a8bc07ffbe4cba053cb1cd26b9a
MatheusRocha12 <112430333+MatheusRocha12@users.noreply.github.com>
2024-02-20 10:31:01 -0300

    Add files via upload

---
+++ Horario.jsx
@@ -0,0 +1,15 @@
+import {
+    lnk
+} from '../App';
+
+export const exibirHorario = (e) => {
+    const dateObj = new Date();
+    const hora = dateObj.getHours();
+    const minuto = dateObj.getMinutes();
+    const segundo = dateObj.getSeconds();
+    let horarioFormatado = hora + ":" + minuto + ":" + segundo;
+    console.clear();
+    console.log(horarioFormatado);
+
+    // lnk.current.innerText = horarioFormatado;
+}
